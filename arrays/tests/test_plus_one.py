cd4dac7675267521250f8723bc0c303c1b70383b
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-01-29 18:13:24 -0800

    Complete Plus One algorithm

---
+++ arrays/tests/test_plus_one.py
@@ -0,0 +1,8 @@
+from arrays.plus_one import plus_one, plus_one2
+
+
+def test_plus_one():
+    assert plus_one([1, 2, 3]) == [1, 2, 4]
+    assert plus_one([0]) == [1]
+    assert plus_one([9, 9]) == [1, 0, 0]
+
