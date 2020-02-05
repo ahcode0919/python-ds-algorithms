3f4316d2c1afab616cc9434c4e6aac5028431c95
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-02-04 19:40:00 -0800

    Diagonal traverse

--- multi_dimensional_arrays/README.md
+++ multi_dimensional_arrays/README.md
@@ -15,6 +15,34 @@ Input:
 ```
 Output: `1, 2, 4, 7, 5, 3, 6, 8, 9`
 
+```python
+def diagonal_traverse(matrix: List[List[int]]) -> List[int]:
+    result = []
+    temp = []  # to reverse odd rows
+
+    if not matrix or not matrix[0]:
+        return result
+
+    height, length = len(matrix), len(matrix[0])
+    diagonals = range(height + length - 1)
+
+    for diagonal in diagonals:
+        row = 0 if diagonal < length else diagonal - length + 1
+        col = diagonal if diagonal < length else length - 1
+
+        while row < height and col > -1:
+            temp.append(matrix[row][col])
+            row += 1
+            col -= 1
+
+        if diagonal % 2 == 0:
+            result.extend(temp[::-1])
+        else:
+            result.extend(temp)
+        temp.clear()
+
+    return result
+```
 
 
 ## Rotate Image 90 Degrees Clockwise
--- multi_dimensional_arrays/diagonal_traverse.py
+++ multi_dimensional_arrays/diagonal_traverse.py
@@ -1,17 +1,41 @@
 from typing import List
-# Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order.
-# #
-# # Input:
-# # ```
-# #     [[1, 2, 3],
-# #      [4, 5, 6],
-# #      [7, 8, 9]]
-# # ```
-# # Output: `1, 2, 4, 7, 5, 3, 6, 8, 9`
+
 
 def diagonal_traverse(matrix: List[List[int]]) -> List[int]:
-    start = [0, 0]
-    end = [len(matrix) - 1, len(matrix[0])]
-    current = start[::1]
-    increments = 0
+    """
+    Given a matrix of M x N elements (M rows, N columns), return all elements of the matrix in diagonal order.
 
+    Input:
+    [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+    Output: 1, 2, 4, 7, 5, 3, 6, 8, 9
+    :param matrix: square matrix
+    :return: list of diagonal transversed values
+    """
+    result = []
+    temp = []  # to reverse odd rows
+
+    if not matrix or not matrix[0]:
+        return result
+
+    height, length = len(matrix), len(matrix[0])
+    diagonals = range(height + length - 1)
+
+    for diagonal in diagonals:
+        row = 0 if diagonal < length else diagonal - length + 1
+        col = diagonal if diagonal < length else length - 1
+
+        while row < height and col > -1:
+            temp.append(matrix[row][col])
+            row += 1
+            col -= 1
+
+        if diagonal % 2 == 0:
+            result.extend(temp[::-1])
+        else:
+            result.extend(temp)
+        temp.clear()
+
+    return result
