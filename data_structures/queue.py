6cdede1414d135dc897f8ea5efa02fe710aaa49c
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-02-06 15:26:51 -0800

    Update linting and generics

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -1,20 +1,22 @@
-from typing import Generic
+from typing import Generic, TypeVar, List
+
+T = TypeVar('T')
 
 
-class QueueList:
+class QueueList(Generic[T]):
     """
     Basic queue backed with a list. Not optimal since list has O(N) complexity for
     inserting and removing elements at the beginning
     """
 
     def __init__(self):
-        self.queue = []
+        self.queue: List[T] = []
 
     def __len__(self):
         return len(self.queue)
 
-    def dequeue(self) -> Generic:
+    def dequeue(self) -> T:
         return self.queue.pop(0)
 
-    def enqueue(self, element: Generic):
+    def enqueue(self, element: T):
         self.queue.append(element)
