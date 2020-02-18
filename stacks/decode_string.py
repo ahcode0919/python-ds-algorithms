20609e938364ecad19f3a84153b3291991d794cb
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-02-17 17:44:38 -0800

    Decode String

--- stacks/decode_string.py
+++ stacks/decode_string.py
@@ -1,35 +1,20 @@
-from collections import deque
-from typing import Deque
 
 
 def decode_string(value: str) -> str:
-    group = 0
-    output: Deque = deque()
-    stack: Deque = deque()
-
-    for char in reversed(value):
-        if char in ']':
-            group += 1
-            continue
-
-        if char in '[':
-            continue
-
-        if char.isdigit():
-            multiplier = int(char)
-            chars = []
-            group -= 1
-
-            for _ in range(len(stack)):
-                chars.append(stack.pop())
-
-            if group == 0:
-                output.extendleft(reversed(chars * multiplier))
-            else:
-                stack.extend(chars * multiplier)
+    stack = []
+    for char in value:
+        if char != ']':
+            stack.append(char)
         else:
-            if group == 0:
-                output.appendleft(char)
-            else:
-                stack.append(char)
-    return ''.join(list(output))
+            temp_string, num = '', ''
+            while stack and stack[-1] != '[':
+                temp_string = stack.pop() + temp_string
+
+            stack.pop()
+            while stack and stack[-1].isdigit():
+                num = stack.pop() + num
+
+            num = int(num)
+            stack.append(temp_string * num)
+
+    return ''.join(stack)
--- stacks/tests/test_decode_string.py
+++ stacks/tests/test_decode_string.py
@@ -5,4 +5,3 @@ def test_decode_string():
     assert decode_string("3[a]2[bc]") == "aaabcbc"
     assert decode_string("3[a2[c]]") == "accaccacc"
     assert decode_string("2[abc]3[cd]ef") == "abcabccdcdcdef"
-
