33e5064bdeb1f2118b073c604038d1878c2130e1
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-03-10 10:16:30 -0700

    Add testcase

--- doubly_linked_list/tests/test_flatten_doubly_linked_lists.py
+++ doubly_linked_list/tests/test_flatten_doubly_linked_lists.py
@@ -1,4 +1,4 @@
-from doubly_linked_list.flatten_doubly_linked_lists import flatten_iterative
+from doubly_linked_list.flatten_doubly_linked_lists import flatten, flatten_iterative
 from doubly_linked_list.flatten_doubly_linked_lists import Node
 from test_helpers.test_helpers import get_list_values
 
@@ -10,4 +10,5 @@ def test_flatten():
     head.next = Node(2, head, None, child)
     head.next.next = Node(5, head.next, None, None)
 
+    assert get_list_values(flatten(head)) == [1, 2, 3, 4, 5]
     assert get_list_values(flatten_iterative(head)) == [1, 2, 3, 4, 5]
