287c86c9fc73209f6d69dc1aeaa269aca24458bd
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-03-10 18:47:43 -0700

    Add Reverse In-place

--- strings/tests/test_reverse_string.py
+++ strings/tests/test_reverse_string.py
@@ -1,8 +1,14 @@
-from strings.reverse_string import reverse_string, reverse_string_with_list_comprehension, reverse_string_with_loop
+from strings.reverse_string import reverse_string, reverse_string_in_place, reverse_string_with_list_comprehension, reverse_string_with_loop
 
 
 def test_reverse_string():
     assert reverse_string('test') == 'tset'
+
+
+def test_reverse_string_inplace():
+    string = ['t', 'e', 's', 't']
+    reverse_string_in_place(string)
+    assert string == ['t', 's', 'e', 't']
 
 
 def test_reverse_string_with_list_comprehension():
