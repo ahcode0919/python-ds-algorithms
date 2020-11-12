0ae84387b273056ee4540ce00f449e04daae14b4
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-11-12 02:05:26 -0800

    Fix Linting Error

--- n_ary_tree/tests/test_postorder_traversal.py
+++ n_ary_tree/tests/test_postorder_traversal.py
@@ -5,4 +5,3 @@ from test_helpers.test_helpers import get_n_nary_tree
 def test_postorder_traversal():
     assert postorder_traversal(get_n_nary_tree()) == [7, 5, 6, 2, 8, 3, 9, 10, 12, 11, 4, 1]
     assert postorder_traversal(None) == []
-
