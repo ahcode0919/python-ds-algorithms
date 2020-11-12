a3f5f3ee02290228150142ff6b2be3ad07fc85d2
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2020-11-12 02:37:06 -0800

    Max Depth

---
+++ n_ary_tree/max_depth.py
@@ -0,0 +1,32 @@
+from n_ary_tree.node import Node
+
+
+def max_depth_top_down(root: Node) -> int:
+    def traverse(node, depth):
+        if not node:
+            return depth
+
+        maximum_depth = depth
+
+        if node.children:
+            for child in node.children:
+                maximum_depth = max(maximum_depth, traverse(child, depth + 1))
+        return maximum_depth
+
+    if not root:
+        return 0
+
+    return traverse(root, 1)
+
+
+def max_depth_bottom_up(root: Node) -> int:
+    if not root:
+        return 0
+
+    depth = 1
+
+    if root.children:
+        for child in root.children:
+            depth = max(depth, max_depth_bottom_up(child) + 1)
+
+    return depth
