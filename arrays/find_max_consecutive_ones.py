6e7a0dabb3d1202d913e5a77afc2fa9b84bb3a8a
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2026-03-19 14:15:25 -0700

    fix linter errors

--- arrays/find_max_consecutive_ones.py
+++ arrays/find_max_consecutive_ones.py
@@ -1,11 +1,12 @@
 from typing import List
 
+
 def find_max_consecutive_ones(nums: List[int]) -> int:
     max_ones = 0
 
     if not nums:
         return max_ones
-    
+
     ones_count = 0
 
     for num in nums:
