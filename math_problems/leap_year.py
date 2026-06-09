5430c2bffd59f0c5c8b2870b0048e08d522a82a5
Aaron H <7063649+ahcode0919@users.noreply.github.com>
2026-06-09 11:12:26 -0700

    Fix linter error

--- math_problems/leap_year.py
+++ math_problems/leap_year.py
@@ -7,7 +7,7 @@ def leap_year(year: int) -> bool:
     The year can be evenly divided by 4, is a leap year, unless:
     The year can be evenly divided by 100, it is NOT a leap year, unless:
     The year is also evenly divisible by 400. Then it is a leap year.
-    This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, 
+    This means that in the Gregorian calendar, the years 2000 and 2400 are leap years,
     while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. Source
     """
     leap = False
