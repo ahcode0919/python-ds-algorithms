bf906cbf409feba624ab21f328cf1e20a4d2f639
Aaron <7063649+ahcode0919@users.noreply.github.com>
2026-07-26 11:21:18 -0700

    Add min heap data structure

---
+++ data_structures/min_heap.py
@@ -0,0 +1,35 @@
+class MinHeap:
+
+    def __init__(self):
+        self.list = []
+
+    def insert(self, val):
+        self.list.append(val)
+        self._sift_up(len(self.list) - 1)
+    
+    def is_empty(self):
+        return len(self.list) == 0
+
+    def size(self):
+        return len(self.list)
+
+    def values(self):
+        values = []
+        for value in self.list:
+            values.append(value)
+        return values
+
+    def _get_parent(self, idx):
+        return (idx - 1) // 2
+
+    def _sift_up(self, idx):
+        current_index = idx
+
+        while current_index > 0:
+            parent = self._get_parent(current_index)
+
+            if self.list[current_index] < self.list[parent]:
+                self.list[current_index], self.list[parent] = self.list[parent], self.list[current_index]
+                current_index = parent
+            else:
+                break
---
+++ tests/data_structures/test_min_heap.py
@@ -0,0 +1,50 @@
+from data_structures.min_heap import MinHeap
+
+def test_min_heap_init():
+    min_heap = MinHeap()
+    assert min_heap.size() == 0
+
+def test_min_heap_insert():
+    min_heap = MinHeap()
+    min_heap.insert(12)
+    min_heap.insert(13)
+    min_heap.insert(11)
+    min_heap.insert(4)
+    min_heap.insert(20)
+    min_heap.insert(9)
+    min_heap.insert(22)
+    min_heap.insert(14)
+    assert min_heap.values() == [4, 11, 9, 13, 20, 12, 22, 14]
+
+def test_min_heap_is_empty():
+    min_heap = MinHeap()
+    assert min_heap.is_empty()
+
+    min_heap.insert(1)
+    assert not min_heap.is_empty()
+
+def test_min_heap_size():
+    min_heap = MinHeap()
+    assert min_heap.size() == 0
+
+    min_heap.insert(12)
+    assert min_heap.size() == 1
+
+    min_heap.insert(13)
+    assert min_heap.size() == 2
+
+    min_heap.insert(11)
+    assert min_heap.size() == 3
+
+def test_min_heap_values():
+    min_heap = MinHeap()
+    assert min_heap.values() == []
+
+    min_heap.insert(12)
+    assert min_heap.values() == [12]
+
+    min_heap.insert(13)
+    assert min_heap.values() == [12, 13]
+
+    min_heap.insert(11)
+    assert min_heap.values() == [11, 13, 12]
